0e6052d30c716aab525ea60c86816c317e6a1180
Kirke <kirkekr@gmail.con>
2018-02-20 18:46:34 +0200

    Initial commit

---
+++ src/main/java/ee/ttu/idk0071/financehelpers/FinancehelpersApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.idk0071.financehelpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancehelpersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancehelpersApplication.class, args);
+	}
+}
---
+++ src/test/java/ee/ttu/idk0071/financehelpers/FinancehelpersApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.ttu.idk0071.financehelpers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinancehelpersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
