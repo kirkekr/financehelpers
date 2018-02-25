ac933711940929d43570fe9708da46b4bb8f2d0a
Kirke <kirkekr@gmail.con>
2018-02-25 23:52:53 +0200

    Add statistics page and login page, very basic. Tried routing

--- financehelpers-web/src/app.js
+++ financehelpers-web/src/app.js
@@ -1,5 +1,11 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: 'home/index' },
+      { route: 'login', name: 'login', moduleId:'users/login', nav:true, title:'Login' }
+    ]);
   }
+
 }
---
+++ financehelpers-web/src/users/login.js
@@ -0,0 +1,6 @@
+export class Login{
+  constructor() {
+    this.message = 'Hello Aurelia!';
+  }
+
+}
