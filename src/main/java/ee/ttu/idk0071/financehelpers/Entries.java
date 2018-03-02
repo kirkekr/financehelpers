e95ee01d61f1b68f8a2259229d482626a7646b21
Kirke <kirkekr@gmail.con>
2018-03-02 15:44:37 +0200

    Repair database. Gradle runs

--- src/main/java/ee/ttu/idk0071/financehelpers/Entries.java
+++ src/main/java/ee/ttu/idk0071/financehelpers/Entries.java
@@ -1,15 +1,19 @@
 package ee.ttu.idk0071.financehelpers;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
 import java.util.Date;
+
 
 @Data
 @Entity
+@Getter
+@Setter
 public class Entries {
 
-    @Id
    private int ID;
     private String EntryName;
     private String Username;
