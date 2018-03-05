e400a6ff2f22fc244d9451f64a68b1ec0f7ea4c7
Kirke <kirkekr@gmail.con>
2018-03-05 23:18:16 +0200

    Aurelia works. Just plain pages. New navigation bar.

--- financehelpers-web/src/app.js
+++ financehelpers-web/src/app.js
@@ -9,9 +9,9 @@ export class App {
     this.router = router;
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'home'], name: 'home', moduleId: 'home/index' },
-      { route: 'users/login', name: 'login', moduleId:'users/login', nav:true, title:'Login' },
-      {route: 'home/statistics', name:'statistics', moduleId:'home/statistics', title:'Statistics', nav:true}
+      { route: ['', 'home'], name: 'home', moduleId: 'home/index', nav:true , title:'Home'},
+      { route: 'logins', name: 'login', moduleId:'users/login', nav:true, title:'Login' },
+      {route: 'stats', name:'statistics', moduleId:'home/statistics', title:'Statistics', nav:true}
     ]);
   }
 
--- financehelpers-web/src/home/index.js
+++ financehelpers-web/src/home/index.js
@@ -1,11 +1,3 @@
 export class Index{
-configureRouter(config, router) {
-    this.router = router;
-    config.title = "Finance Helper";
 
-    config.map([
-      { route: ["","home"], name: "home", moduleId: "home/index" },
-      { route: "people", name: "people", moduleId: "people/people", nav: true}
-    ]);
-  }
 }
