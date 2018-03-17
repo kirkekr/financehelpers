7dbdc460607952aa76799e6029ee4a411c46e584
Kirke <kirkekr@gmail.con>
2018-03-18 00:10:46 +0200

    Indexpage timeline uus entry avab.

--- financehelpers-web/src/home/index.js
+++ financehelpers-web/src/home/index.js
@@ -1,3 +1,15 @@
+import {HttpClient, json} from 'aurelia-fetch-client';
+
+let client = new HttpClient();
+
 export class Index{
 
+   openNew() {
+    var x = document.getElementById("demo");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 }
