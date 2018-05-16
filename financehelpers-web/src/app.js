1fa31fa1b4935bf20cf54cf5403d2413961acd47
Kirke <kirkekr@gmail.con>
2018-05-16 13:01:59 +0300

    Remove unnecessary code

--- financehelpers-web/src/app.js
+++ financehelpers-web/src/app.js
@@ -5,7 +5,6 @@ export class App {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'home'], name: 'home', moduleId: 'home/index', nav:true , title:'Home'},
-      { route: 'logins', name: 'login', moduleId:'users/login', nav:true, title:'Login' },
       {route: 'stats', name:'statistics', moduleId:'statistics/statistics', title:'Statistics', nav:true},
       {route: 'listing', name:'listing', moduleId:'listing/listing', title:'Listing', nav:true}
     ]);
